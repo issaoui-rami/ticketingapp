ef0c5c3af94ac238338628fbccbce9dace297d35
issaoui-rami <ramiissaoui55@gmail.com>
2020-07-09 11:11:36 +0200

    Updates

---
+++ precache-manifest.cdeb6526a4cce20e111feec137e7d03f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23e8c62463ea526c67c4e419fb52985",
+    "url": "/issaouirami/index.html"
+  },
+  {
+    "revision": "5296e38ca45ebdb2b1fc",
+    "url": "/issaouirami/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c89da2bacdfc1a334021",
+    "url": "/issaouirami/static/css/main.d289369f.chunk.css"
+  },
+  {
+    "revision": "5296e38ca45ebdb2b1fc",
+    "url": "/issaouirami/static/js/2.fbae297e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/issaouirami/static/js/2.fbae297e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89da2bacdfc1a334021",
+    "url": "/issaouirami/static/js/main.09768ef0.chunk.js"
+  },
+  {
+    "revision": "0ca69466d493266fe0c6",
+    "url": "/issaouirami/static/js/runtime-main.54d9366f.js"
+  }
+]);
